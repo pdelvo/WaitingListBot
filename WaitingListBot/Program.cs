c0afe85dba7ca886b176552f407e4452877b397e
Dennis Fischer <dennis.pdelvo@gmail.com>
2021-03-03 18:41:55 +0100

    Use a service

--- WaitingListBot/Program.cs
+++ WaitingListBot/Program.cs
@@ -2,6 +2,9 @@
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.EventLog;
+
 using System;
 using System.IO;
 using System.Linq;
@@ -13,72 +16,21 @@ namespace WaitingListBot
     // Test: https://discord.com/api/oauth2/authorize?client_id=815372800407502902&permissions=68608&scope=bot
     class Program
     {
-        static IServiceCollection serviceCollection;
-
-        public static async Task Main()
+        public static void Main(string[] args)
         {
-            var program = new Program();
-            await program.RunAsync();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        private async Task RunAsync()
-        {
-            var config = new DiscordSocketConfig
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+          Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
             {
-                GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMembers | GatewayIntents.GuildMessages,
-                AlwaysDownloadUsers = true
-            };
-
-            var client = new DiscordSocketClient(config);
-
-            client.Log += Log;
-            client.GuildMemberUpdated += GuildMemberUpdated;
-
-            var token = File.ReadAllText("token.txt");
-
-            await client.LoginAsync(TokenType.Bot, token);
-            await client.StartAsync();
-            await client.SetGameAsync("Rocket League");
-            await client.SetActivityAsync(new Game("Rocket League", ActivityType.Watching));
-
-            // Commands are not thread safe. So set the run mode to sync
-            var commandService = new CommandService(new CommandServiceConfig { DefaultRunMode = RunMode.Sync, LogLevel = LogSeverity.Info });
-
-            serviceCollection = new ServiceCollection();
-            serviceCollection.AddSingleton(typeof(CommandService), commandService);
-            serviceCollection.AddSingleton(typeof(StorageFactory), new StorageFactory());
-
-            var handler = new CommandHandler(client, commandService, serviceCollection.BuildServiceProvider());
-
-            await handler.InstallCommandsAsync();
-
-            // Block this task until the program is closed.
-            await Task.Delay(-1);
-        }
-
-        private Task GuildMemberUpdated(SocketGuildUser before, SocketGuildUser after)
-        {
-            // Try to update
-            var storageFactory = serviceCollection.BuildServiceProvider().GetService<StorageFactory>();
-
-            var storage = storageFactory.GetStorage(after.Guild.Id);
-
-            foreach (var user in storage.List)
-            {
-                if (user.Id == after.Id)
+                services.AddHostedService<Worker>()
+            .Configure<EventLogSettings>(config =>
                 {
-                    user.Name = after.Nickname ?? after.Username;
-                    user.IsSub = after.Roles.Any(x => x.Id == storage.SubRoleId);
-                }
-            }
-
-            return Task.CompletedTask;
-        }
-
-        private Task Log(LogMessage logMessage)
-        {
-            Console.WriteLine(logMessage.ToString());
-            return Task.CompletedTask;
-        }
+                    config.LogName = "Sample Service";
+                    config.SourceName = "Sample Service Source";
+                });
+            });
     }
 }
---
+++ WaitingListBot/Worker.cs
@@ -0,0 +1,85 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaitingListBot
+{
+    internal class Worker : IHostedService
+    {
+        static IServiceCollection serviceCollection;
+        DiscordSocketClient client;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var config = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMembers | GatewayIntents.GuildMessages,
+                AlwaysDownloadUsers = true
+            };
+
+            client = new DiscordSocketClient(config);
+
+            client.Log += Log;
+            client.GuildMemberUpdated += GuildMemberUpdated;
+
+            var token = File.ReadAllText("token.txt");
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            await client.SetGameAsync("Rocket League");
+            await client.SetActivityAsync(new Game("Rocket League", ActivityType.Watching));
+
+            // Commands are not thread safe. So set the run mode to sync
+            var commandService = new CommandService(new CommandServiceConfig { DefaultRunMode = RunMode.Sync, LogLevel = LogSeverity.Info });
+
+            serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton(typeof(CommandService), commandService);
+            serviceCollection.AddSingleton(typeof(StorageFactory), new StorageFactory());
+
+            var handler = new CommandHandler(client, commandService, serviceCollection.BuildServiceProvider());
+
+            await handler.InstallCommandsAsync();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return client.StopAsync();
+        }
+
+        private Task GuildMemberUpdated(SocketGuildUser before, SocketGuildUser after)
+        {
+            // Try to update
+            var storageFactory = serviceCollection.BuildServiceProvider().GetService<StorageFactory>();
+
+            var storage = storageFactory.GetStorage(after.Guild.Id);
+
+            foreach (var user in storage.List)
+            {
+                if (user.Id == after.Id)
+                {
+                    user.Name = after.Nickname ?? after.Username;
+                    user.IsSub = after.Roles.Any(x => x.Id == storage.SubRoleId);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task Log(LogMessage logMessage)
+        {
+            Console.WriteLine(logMessage.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
