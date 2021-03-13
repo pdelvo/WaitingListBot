9a087de9ebc6699bdd162ccced593353eaf8bed2
Dennis Fischer <dennis.pdelvo@gmail.com>
2021-03-13 02:53:18 +0100

    Update Website

---
+++ WaitingListBot.Web/Pages/Setup.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WaitingListBot.Web.Pages
+{
+    public class SetupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
