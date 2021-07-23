53926530ab5def1598df1a441f14131e4534d655
Dennis Fischer <dennis.pdelvo@gmail.com>
2021-07-24 01:49:00 +0200

    Add database changes

--- WaitingListBot/Migrations/20210723234851_AddInviteRole.Designer.cs
+++ WaitingListBot/Migrations/20210723234851_AddInviteRole.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WaitingListBot.Data;
 
 namespace WaitingListBot.Migrations
 {
     [DbContext(typeof(WaitingListDataContext))]
-    partial class WaitingListDataContextModelSnapshot : ModelSnapshot
+    [Migration("20210723234851_AddInviteRole")]
+    partial class AddInviteRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WaitingListBot/Migrations/20210723234851_AddInviteRole.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WaitingListBot.Migrations
+{
+    public partial class AddInviteRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "InviteRole",
+                table: "Invites",
+                type: "bigint unsigned",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsInviteRolePositive",
+                table: "Invites",
+                type: "tinyint(1)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InviteRole",
+                table: "Invites");
+
+            migrationBuilder.DropColumn(
+                name: "IsInviteRolePositive",
+                table: "Invites");
+        }
+    }
+}
